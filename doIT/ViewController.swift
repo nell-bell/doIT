26cdab6de35d5a02c4172ecb90ada5d21c0fce65
julia <julianellie2@gmail.com>
2020-06-23 14:14:28 -0500

    initial

--- doIT/ViewController.swift
+++ doIT/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBOutlet weak var label: UILabel!
+    
 
 
 }
