ef20ba10d358bf1ce77b76941c2cf33bc4beb541
Kelly Tu <kelly22004t@gmail.com>
2020-06-25 12:37:53 -0500

    connected buttons/text/labels

--- doIT/AddToDoViewController.swift
+++ doIT/AddToDoViewController.swift
@@ -12,26 +12,29 @@ class AddToDoViewController: UIViewController {
     
     var previousVC = ToDoTableViewController()
     
+    
     @IBOutlet weak var titleTextField: UITextField!
+    
     @IBOutlet weak var importantSwitch: UISwitch!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-    
-    
+
     @IBAction func addedTapped(_ sender: Any) {
-        
         let toDo = ToDo()
         
         if let titleText = titleTextField.text{
-            toDo.name = titleText
-            toDo.important = importantSwitch.isOn
-        }
-        previousVC.toDos.append(toDo)
-        previousVC.tableView.reloadData()
-        navigationController?.popViewController(animated: true)
+        toDo.name = titleText
+        toDo.important = importantSwitch.isOn
+    }
+    previousVC.toDos.append(toDo)
+    previousVC.tableView.reloadData()
+    navigationController?.popViewController(animated: true)
+        
+        
     }
 
     /*
