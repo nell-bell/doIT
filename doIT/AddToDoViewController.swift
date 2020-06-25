1bf49ffdc1b6328dd76b7a981a0d09f921c0f109
Kelly Tu <kelly22004t@gmail.com>
2020-06-25 12:03:25 -0500

    removed orig view controller

--- doIT/AddToDoViewController.swift
+++ doIT/AddToDoViewController.swift
@@ -18,7 +18,6 @@ class AddToDoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
     
--- doIT/CompleteToDoViewController.swift
+++ doIT/CompleteToDoViewController.swift
@@ -17,7 +17,6 @@ class CompleteToDoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         titleLabel.text = selectedToDo.name
     }
     
